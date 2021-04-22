f62a4b92c5702edc69e591316944ca83a9a52df8
Julia Ramos <juliaramosguedes@gmail.com>
2021-04-22 11:05:10 -0300

    refac

--- cache.js
+++ cache.js
@@ -1,17 +1,14 @@
 const NodeCache = require('node-cache');
 const myCache = new NodeCache({ maxKeys: 10, checkperiod: 60, stdTTL: 60 });
 
-const cache = (fn) => {
+const cache = (fn, params) => (...args) => {
+  const { maxAge, key } = params;
+  const data = myCache.get(key);
+  if (data) return args[0].res.json(data);
 
-  return function (...args) {
-    const key = args[0].originalUrl;
-    const data = myCache.get(key);
-    if (data) return args[0].res.json(data);
-
-    const call = fn(...args);
-    const cachedSucceed = myCache.set(key, call, 300);
-    if (cachedSucceed) return call;
-  };
+  const call = fn(...args);
+  const cachedSucceed = myCache.set(key, call, maxAge);
+  if (cachedSucceed) return call;
 };
 
 module.exports = cache;
--- index.js
+++ index.js
@@ -1,18 +1,10 @@
 const express = require('express');
-const cache = require('./cache');
+const routes = require('./routes');
+
 const app = express();
-
-let route = (request, response) => {
-  console.log('chamou a rota');
-  const result = ["a", "b", "c"];
-  response.json(result);
-  return result;
-}
-
-route = cache(route);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.get('/', route);
+app.get('/', routes.Query.getList);
 
 app.listen('3333',() => console.log('App running on port 3333'));
